fc8cade1ba3efea9f253dd6ad872d1dab001fb7c
AhmedFadel17 <ahmedfadel1717@gmail.com>
2026-03-29 22:08:04 +0200

    fix(web): fix the identity server url in frontend

---
+++ VidiMetrics.Web/src/api/authConfig.ts
@@ -0,0 +1,5 @@
+export const AuthConfig = {
+    IdentityServerUrl: import.meta.env.VITE_IDENTITY_SERVER_URL,
+    ClientId: import.meta.env.VITE_IDENTITY_CLIENT_ID,
+    ApiUrl: import.meta.env.VITE_API_URL
+};
