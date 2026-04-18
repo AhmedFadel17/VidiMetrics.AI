2863a4988d45eb1d3de4fa686ac7a0b018e67879
AhmedFadel17 <ahmedfadel1717@gmail.com>
2026-04-18 23:40:07 +0200

    feat(api): add api base controller to extract user id

---
+++ VidiMetrics.API/Controllers/ApiBaseController.cs
@@ -0,0 +1,14 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using VidiMetrics.Domain.Enums;
+
+namespace VidiMetrics.API.Controllers
+{
+    [ApiController]
+    public abstract class ApiBaseController : ControllerBase
+    {
+        protected string CurrentUserId => User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        protected bool IsAdmin => User.IsInRole(UserRole.Admin.ToString());
+        protected Guid CurrentUserGuid => Guid.TryParse(CurrentUserId, out var guid) ? guid : Guid.Empty;
+    }
+}
