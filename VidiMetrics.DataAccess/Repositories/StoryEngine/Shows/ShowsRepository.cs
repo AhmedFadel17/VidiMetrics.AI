182a88bbd1043dac10eea9b2a561635d2ed99fa3
AhmedFadel17 <ahmedfadel1717@gmail.com>
2026-06-10 00:19:58 +0300

    fix(data access): include image with series in repo

--- VidiMetrics.DataAccess/Repositories/StoryEngine/Shows/ShowsRepository.cs
+++ VidiMetrics.DataAccess/Repositories/StoryEngine/Shows/ShowsRepository.cs
@@ -1,18 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using VidiMetrics.DataAccess.Data;
-using VidiMetrics.Domain.Models.StoryEngine;
 using VidiMetrics.DataAccess.Providers.Cashing;
+using VidiMetrics.Domain.Models.StoryEngine;
 
 namespace VidiMetrics.DataAccess.Repositories.StoryEngine.Shows
 {
     public class ShowsRepository : BaseRepository<Show>, IShowsRepository
     {
-        public ShowsRepository(AppDbContext context, ICacheProvider cacheProvider) : base(context,cacheProvider) { }
+        public ShowsRepository(AppDbContext context, ICacheProvider cacheProvider) : base(context, cacheProvider) { }
 
         public async Task<Show?> GetWithDetailsByIdAsync(Guid id)
         {
             return await _context.Shows
                 .Include(s => s.Episodes)
+                .Include(s => s.AiImage)
                 .FirstOrDefaultAsync(s => s.Id == id);
         }
     }
